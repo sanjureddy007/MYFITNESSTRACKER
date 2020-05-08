c652495109518b93f6771b6dbe3d031f1d081959
sanjay reddy m <msanjayreddy007@gmail.com>
2020-05-07 20:45:53 -0400

    commit

---
+++ client/src/views/Excercise.vue
@@ -0,0 +1,58 @@
+<template>
+    <section>
+    <h1 class="title">This is the Workout page</h1>
+    <h2 class="subtitle">You can save progress here</h2>
+        <div class="c1">
+            <figure class ="image is-64x64">
+                <image src="https://www.pexels.com/photo/person-tying-lace-of-shoes-1040427/"></image>
+                </figure>
+               
+        </div>
+        
+    <container>
+        <h2 class="control">Hey guys,u can add the daily workout right here</h2>
+        
+         
+        
+                <input class="input" type="text" placeholder="add excercise" v-model="newex" v-if="isAdmin">
+                    <input class="button" type="submit" value="Add another item" @click="addEx(newex)">
+
+                     <div class="manage" v-for="y in workouts" :key="y.workout">
+                    <h3>{{y.workout}}
+                        <a class="del" @click="remove(i)" v-if="isAdmin"></a>
+                    </h3>    
+            </div>            
+    </container>
+    </section> 
+</template>   
+
+<script>
+import Excercises from "../models/Excercises";
+export default{
+  data:()=>({
+    newex: "",
+    workout: workouts.State.workouts,
+    isAdmin: true
+  }),
+  methods: {
+    
+    async remove(i){
+        try {
+            await workouts.State.workouts.remove(i)
+        } catch (error) {
+            this.error = error
+        }
+        }
+    },
+    async addEx(newex){
+        try {
+            await workouts.State.workouts.addEx(newex)
+        } catch (error) {
+            this.error = error;
+        }
+    },
+    created(){
+        Excercise.Init()
+    }
+  }
+</script>
---
+++ client/src/views/Heart.vue
@@ -0,0 +1,57 @@
+<template> 
+<section class="hero has-background-primary hero is-success is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <figure class="image is-128x128">
+        <img src="https://www.shutterstock.com/image-vector/electrocardiogram-icon-348577334">
+      </figure>
+      <progress class= "progress is-large" :value="BP" max="120/80" ></progress>
+      <h2 class="title"><b>
+          1.Blood pressure
+        
+     </b></h2>
+      <h3 class="subtitle"><b>Enter the Blood Pressure: {{BP}}
+        Unit-mmHg
+      </b></h3>
+      
+    
+                    
+                        <button type="submit" name="bloodpressure" value=" mmHg" @click="add20()" >
+                          </button> 
+              
+                    <button type="submit" name="bloodpressure" value="mmHg" @click="add40()" >
+                    </button>
+      
+      <section class="content">
+          <p>Blood pressure is the force of blood pushing against the walls of the arteries
+              as your heart pumps blood.It includes two measurements."Systolic" is your Blood pressure when your heart
+              beats while pumping blood."Diastolic" is your blood pressure when the heart is at rest between hearts  </p>
+      </section>
+    
+                     
+      <h2 class="title"><b>
+          2.Electrocardiograms
+        
+     </b></h2>
+      <h3 class="subtitle"><b>
+        (ECG or EKG)
+      </b></h3>
+      
+      <section class="content">
+          <p>An Electrocardiogram is a test that records the timing and strength of electrical signals that 
+              make the heart beat.By looking at an ECG,a doctor can gain insights about your heart rhythm and look for irregularities</p>
+      </section>
+    </div>
+  </div>
+</section>
+      
+
+</template>
+<script>
+import Heart from '..Models/Heart'
+export default {
+  data: () => ({
+        BP: Heart.State.Heart[0].BPtotal //index of where you're looking
+    }),
+}
+</script>
